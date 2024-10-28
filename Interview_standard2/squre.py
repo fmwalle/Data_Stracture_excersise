2abfa05d92f335c643d99fb26f4c1ccf460743fb
Fikir Walle <fikirbereketu@gmail.com>
2024-10-28 13:34:35 -0400

    bitwise opertor

--- Interview_standard2/squre.py
+++ Interview_standard2/squre.py
@@ -21,4 +21,30 @@ def mySqrt(x):
                 return res
         return res     
 
-print(mySqrt(2))   
+print(mySqrt(2))  
+
+def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+
+        result=[]
+
+        for i in range(2**n):
+            result.append(i^(i>>1))  
+        return result  
+
+def circularPermutation(self, n, start):
+        """
+        :type n: int
+        :type start: int
+        :rtype: List[int]
+        """
+        result=[]
+        for i in range(2**n):
+            result.append(i^(i>>1))
+
+        start_index=result.index(start)    
+
+        return result[start_index:] + result[:start_index]          
