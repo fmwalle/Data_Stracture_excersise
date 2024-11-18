9edda4146cc4c703336223e723a475f3f3d7fc8b
Fikir Walle <fikirbereketu@gmail.com>
2024-11-18 13:28:48 -0500

    backtracking journey

--- Interview_standard2/remberBST/first_round_prep/backtracking/xor.py
+++ Interview_standard2/remberBST/first_round_prep/backtracking/xor.py
@@ -11,3 +11,49 @@ def xor(nums):
 
 print(xor([5,1,6]))
 
+# https://leetcode.com/problems/subsets/description/
+def subset(nums):
+    result=[]
+    subset=[]
+    def dfs(index):
+        if index >=len(nums):
+            result.append(subset[:])
+            return
+        # descion of include
+        subset.append(nums[index])
+        dfs(index+1)  
+        # descion of exclude
+        subset.pop()
+        dfs(index+1)
+
+    dfs(0)
+    return result
+
+print(subset([1,2,3]))      
+
+
+# https://leetcode.com/problems/combination-sum/description/
+
+def combination_sum(nums,target):
+    result=[]
+    def dfs(index,subset,total):
+        if total==target:
+            result.append(subset[:])
+            return
+        
+        if index>=len(nums) or total>target:
+            return
+        
+        # descion of include
+        subset.append(nums[index])
+        dfs(index,subset,total+nums[index])
+        # descion of exclude
+        subset.pop()
+        dfs(index+1,subset,total)
+
+    dfs(0,[],0)
+    return result
+
+
+print(combination_sum([2,3,6,7],7))
+
