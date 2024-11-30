9ec4416e6164e3ea4fdd6e80ce8c8c8fe400c309
Fikir Walle <fikirbereketu@gmail.com>
2024-11-30 08:05:43 -0500

    three sum

--- Interview_standard2/secondInterviewPrep/twopointer/twosum.py
+++ Interview_standard2/secondInterviewPrep/twopointer/twosum.py
@@ -13,4 +13,35 @@ def twoSum(array,target):
             right-=1       
     return result
 
-print(twoSum([2,7,11,15],9))    
+#print(twoSum([2,7,11,15],9))    
+
+def threeSum(nums):
+    if len(nums)<3:
+        return []
+
+    answer=[]
+    for i in range(0,len(nums)-2):
+        if i>0 and nums[i]==nums[i-1]:
+            continue
+        left=i+1
+        right=len(nums)-1
+
+        while left<right:
+            sum=nums[i]+nums[left]+nums[right]
+            if sum==0:
+                answer.append(nums[i])
+                answer.append(nums[left])
+                answer.append(nums[right])
+                left+=1
+                right-=1
+                while left<right and nums[left]==nums[left-1]:
+                    left+=1
+                while left<right and nums[right]==nums[right+1]:
+                      right-=1
+            elif sum<0:
+                left+=1
+            else:
+                right-=1              
+    return answer
+print(threeSum([-1,0,1,2,-1,-4]))
+
