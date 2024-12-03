95c3969de61af54f771ccce70e2570b7d06d258c
Fikir Walle <fikirbereketu@gmail.com>
2024-12-03 12:27:32 -0500

    two sum day3

--- Interview_standard2/secondInterviewPrep/twopointer/twosum.py
+++ Interview_standard2/secondInterviewPrep/twopointer/twosum.py
@@ -65,3 +65,61 @@ def maxArea(height):
 
         return maximum         
 print(maxArea([1,8,6,2,5,4,8,3,7]))
+
+def firstPalindrome(words):
+    if not words:
+        return ''
+
+    for word in (words):
+        left=0
+        right=len(word)-1
+        while left<right:
+            if word[left]!=word[right]:
+                break
+            left+=1
+            right-=1
+
+        if left>=right:
+            return word
+    return " "   
+
+print(firstPalindrome(["abc","car","ada","racecar","cool"]))      
+print(firstPalindrome(["notapalindrome","racecar"]))  
+print(firstPalindrome(["def","ghi"]))    
+
+def fourSum(nums, target):
+   result=[]
+   nums.sort()
+   for i in range(0,len(nums)-3):
+    if i>0 and nums[i]==nums[i-1]:
+        continue
+    for j in range(i+1,len(nums)-2):
+        if j>i+1 and nums[j]==nums[j-1]:
+            continue
+        left=j+1
+        right=len(nums)-1
+
+        while left<right:
+            total=nums[i]+nums[j]+nums[left]+nums[right]
+            if total==target:
+                result.append([nums[i],nums[j],nums[left],nums[right]])
+
+                while left<right and nums[left]==nums[left+1]:
+                    left+=1
+                while left<right and nums[right]==nums[right-1]:
+                    right-=1
+
+                left+=1
+                right-=1
+            elif total<target:
+                left+=1
+            else:
+                right-=1 
+   return result                                  
+print(fourSum([1,0,-1,0,-2,2],0))             
+
+
+
+
+
+
